94698d67585c9604bac1c5d11fa57d4bb807ce84
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-02 09:15:26 -0700

    Student Percentage Program

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 var names = prompt("Enter Your Name");
-var mathsNumber = +prompt("Enter Your Number");
-var islamiatNumber = +prompt("Enter Your Number");
-var englishNumber = +prompt("Enter Your Number");
-var urduNumber = +prompt("Enter Your Number");
-var totalNumber = (650);
-var studentTotalMarks = (mathsNumber + englishNumber + urduNumber + islamiatNumber);
-var studentPercantage = (studentTotalMarks*100/totalNumber);
-console.log(studentPercantage);
-
